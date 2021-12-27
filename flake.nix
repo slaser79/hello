441e1cdaa6589c93ae762ec185a850fe64ec3782
Shingi Mureriwa <shingi79@hotmail.com>
2021-12-27 12:26:07 +0100

    Changed to use github

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
     description = "A flake for building Hello world";
-    inputs.nixpkgs.url = "path:/home/shingi79/nixpkgs_20211216";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
     outputs = {self, nixpkgs} : {
       defaultPackage.x86_64-linux = 
           with import nixpkgs {system = "x86_64-linux";};
